a2b670ebfdc3f475399c95211be8fa526d92ee86
nahidraz23 <nzamanraz@gmail.com>
2024-04-04 17:59:50 +0600

    CSS updated

--- src/components/BengaliBooks.jsx
+++ src/components/BengaliBooks.jsx
@@ -10,7 +10,7 @@ const BengaliBooks = () => {
             <div className="mb-10">
                 <h1 className="text-4xl font-playfair font-bold text-center text-purple-600"><span className="border-b-2 border-purple-600">Books</span></h1>
             </div>
-            <div className="grid grid-cols-1 lg:grid-cols-3 gap-4">
+            <div className="grid grid-cols-1 lg:grid-cols-3 gap-4 mb-4">
                 {
                     bengaliBooks.map(book => <BengaliBook key={book.bookId} book={book}></BengaliBook>)
                 }
